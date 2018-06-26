38be829c754baa31649faf9cb72173b84f8b186a
dangbing <bing.dang@zebra-c.com>
2018-06-26 14:44:42 +0800

    delete cart

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -4,9 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\AddCartRequest;
 use App\Models\CartItem;
+use Illuminate\Http\Request;
+use App\Models\ProductSku;
 
 class CartController extends Controller
 {
+    public function index(Request $request)
+    {
+        $cartItems = $request->user()->cartItems()->with(['productSku.product'])->get();
+
+        return view('cart.index', ['cartItems' => $cartItems]);
+    }
+
     public function add(AddCartRequest $request)
     {
         $user   = $request->user();
@@ -28,6 +37,13 @@ class CartController extends Controller
             $cart->productSku()->associate($skuId);
             $cart->save();
         }
+
+        return [];
+    }
+
+    public function remove(ProductSku $sku, Request $request)
+    {
+        $request->user()->cartItems()->where('product_sku_id', $sku->id)->delete();
 
         return [];
     }
