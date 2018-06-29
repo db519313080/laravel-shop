c19b9cc6b9d606481a14425eae2c26ddc9b45f07
dangbing <bing.dang@zebra-c.com>
2018-06-29 09:29:15 +0800

    asd

--- app/User.php
+++ app/User.php
@@ -6,6 +6,7 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use App\Models\Product;
 use App\Models\CartItem;
+//
 
 class User extends Authenticatable
 {
